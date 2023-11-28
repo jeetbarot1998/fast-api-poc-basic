0e743d77da7ef8f8dbf5a563a78c47250752488e
Jeet Barot <jeetbarot1998@gmail.com>
2023-11-28 19:37:36 +0530

    changing host url

--- runserver.py
+++ runserver.py
@@ -4,7 +4,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8080,
         log_level="info",
         reload=True
